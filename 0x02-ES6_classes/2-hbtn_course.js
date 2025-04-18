047000c68a9298db7e11beec01442dec62838356
Yassine OUJAMA <yassine.oujama@gmail.com>
2025-04-18 16:13:35 +0100

    add 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,42 +1,39 @@
-class ALXCourse {
+export default class ALXCourse {
     constructor(name, length, students) {
-        if (typeof name !== 'string') {
-            throw new Error('Name must be a string');
-        }
-        if (typeof length !== 'number') {
-            throw new Error('Length must be a number');
-        }
-        if (!Array.isArray(students)) {
-            throw new Error('Students must be an array');
-        }
-
-        this._name = name;
-        this._length = length;
-        this._students = students;
+      this.name = name;
+      this.length = length;
+      this.students = students;
     }
-
+  
     get name() {
-        return this._name;
+      return this._name;
     }
-
+  
+    set name(val) {
+      if (typeof val !== 'string') { throw new TypeError('Name must be a string'); }
+      this._name = val;
+    }
+  
     get length() {
-        return this._length;
+      return this._length;
     }
-
+  
+    set length(val) {
+      if (typeof val !== 'number') { throw new TypeError('Length must be a number'); }
+      this._length = val;
+    }
+  
     get students() {
-        return this._students;
+      return this._students;
     }
-
-    set name(name) {
-        this._name = name;
+  
+    set students(val) {
+      if (!Array.isArray(val)) { throw new TypeError('Students must be an array'); }
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < val.length; i++) {
+        if (typeof val[i] !== 'string') { throw new TypeError('Students must be an array of strings'); }
+      }
+      this._students = val;
     }
-
-    set length(length) {
-        this._length = length;
-    }
-
-    set students(students) {
-        this._students = students;
-    }
-}
-export default ALXCourse;
+  }
+  
